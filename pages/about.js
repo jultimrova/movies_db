322b5dda8785746387d5646f6da35adf2c4a0ca6
arvein <moonflower019@gmail.com>
2020-06-23 18:05:05 +0300

    create reusable components

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const About = () => {
+    const message = 'Hello world'
+    return (
+        <h1>This is about page - {message}</h1>
+    )
+}
+
+export default About
