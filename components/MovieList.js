40d3d9026df61c96ccaad6c9eedf38c91833e57a
arvein <moonflower019@gmail.com>
2020-06-28 12:55:34 +0300

    hooks with async operations

--- components/MovieList.js
+++ components/MovieList.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 class MovieList extends React.Component {
+
+    shorten = (text, maxLength) => {
+        if (text && text.length > maxLength) {
+            return text.substr(0, maxLength) + '...'
+        }
+        return text
+    }
+
     renderMovies(movies) {
         return movies.map(movie => (
-                <div className="col-lg-4 col-md-6 mb-4">
+                <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
                     <div className="card h-100">
                         <a href="#"><img className="card-img-top" src={movie.image} alt=""/></a>
                         <div className="card-body">
@@ -11,7 +19,7 @@ class MovieList extends React.Component {
                                 <a href="#">{movie.name}</a>
                             </h4>
                             <h5>$24.99</h5>
-                            <p className="card-text">{movie.description}</p>
+                            <p className="card-text">{this.shorten(movie.description, 200)}</p>
                         </div>
                         <div className="card-footer">
                             <small className="text-muted">{movie.rating}</small>
